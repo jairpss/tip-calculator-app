ec9a68072e23f2c9be9c154fc86c5c71c75b5efa
Jair Partida <jairpartida1@gmail.com>
2021-08-10 23:40:27 -0600

    create the vars

---
+++ main.js
@@ -0,0 +1,10 @@
+let tip, bill, numPeople;
+
+let billInput = document.querySelector('.input-bill');
+let peopleInput = document.querySelector('.input-people');
+let tipOptions = document.querySelector(".tip-percent-grid").querySelectorAll("label");
+let tipCustom  = document.querySelector(".custom");
+let resetBtn = document.querySelector("button");
+
+let tipAmount = document.querySelector(".text-tip-amount");
+let total = document.querySelector(".text-total");
