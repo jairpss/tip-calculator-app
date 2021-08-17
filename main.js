e1277f5ea1bc8a7e8fb794b9d62b01cf26ab8699
Jair Partida <jairpartida1@gmail.com>
2021-08-16 21:25:57 -0600

    adding some functions

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ let resetBtn = document.querySelector("button");
 
 let tipAmount = document.querySelector(".text-tip-amount");
 let total = document.querySelector(".text-total");
+
+//Logical operations for the tips
+const calculate_tip = (Bill, Tip, PeopleNumber) => {
+    let tip1, tip2;
+    tip1 = Math.round(((Number(Bill) * (Number(Tip) / 100)) / Number(PeopleNumber)) * 100) / 100;
+    tip2 = Math.round(((Number(Bill) / Number(PeopleNumber)) + tip1) * 100) / 100;
+    tipAmount.innerText = `$${tip1}`;
+    total.innerText = `$${total}`
+}
+
+//Reset all radio inputs to false
+const radio_inputs = () => {
+    document.querySelectorAll("[type='radio']").forEach(elem => elem.checked = false);
+}
+
+//Reset button
+resetBtn.addEventListener("click", () => {
+    bill, tip, numPeople = 0;
+    [billInput , peopleInput ,tipCustom].map( elem => elem.value = " ");
+    [tipAmount,tolal].map( elem => elem.innerText = "$0.00")
+    radio_inputs()
+    resetBtn.disabled = true 
+});
