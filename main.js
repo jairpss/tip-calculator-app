1ecad0dbcd7150da806e306fa4717c9858d59935
Jair Partida <jairpartida1@gmail.com>
2021-10-01 23:39:51 -0600

    still missing reset the inputs

--- main.js
+++ main.js
@@ -6,13 +6,19 @@ const errorMsg = document.querySelector('.error-msg');
 const results = document.querySelectorAll('.value');
 const resetBtn = document.querySelector('.reset');
 
+bill.addEventListener('input', setBillValue);
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', handleClick);
+});
+tipCustom.addEventListener('input', setTipCustomValue);
+people.addEventListener('input', setPeopleValue);
 resetBtn.addEventListener('click', reset);
 
 let billValue = 0.0; //default value
 let tipValue = 0.15; //default value -> 15% button is active
 let peopleValue = 1; 
 
-console.log(bill.value);
+
 
 function validateFloat(s){
     var rgx = /^[0-9]*\.?[0-9]*$/;
@@ -24,7 +30,7 @@ function validateInt(s){
     return s.match(rgx);
 }
 
-const setBillValue = bill.addEventListener('input', () => {
+function setBillValue() {  
     if (bill.value.includes(',')){
         bill.value = bill.value.replace(',', '.');
     }
@@ -36,12 +42,9 @@ const setBillValue = bill.addEventListener('input', () => {
     billValue = parseFloat(bill.value);
 
     calculateTip();
-    console.log(bill.value);
-});
+}
 
-
-tipBtns.forEach(btn => {
-    btn.addEventListener('click', (event) => {
+function handleClick(event) {
         tipBtns.forEach(btn => {
             //clear active state
             btn.classList.remove('btn-active');
@@ -57,10 +60,9 @@ tipBtns.forEach(btn => {
         tipCustom.value = '';
     
         calculateTip();
-    });
-});
+};
 
-tipCustom.addEventListener('input', () => {
+function setTipCustomValue() {
     if(!validateInt(tipCustom.value)){
         tipCustom.value = tipCustom.value.substring(0, tipCustom.value.length-1);
     }
@@ -75,24 +77,24 @@ tipCustom.addEventListener('input', () => {
     if(tipCustom.value !== ''){
         calculateTip();
     }
-});
+};
 
-const setPeopleValue = people.addEventListener('input', () => {
-    if(!validateInt(people.value)){
-        people.value = people.value.substring(0, people.value.length-1);
-    }
+function setPeopleValue() { 
+        if(!validateInt(people.value)){
+            people.value = people.value.substring(0, people.value.length-1);
+        }
 
-    peopleValue = parseFloat(people.value);
+        peopleValue = parseFloat(people.value);
 
-    if(peopleValue <= 0){
-        errorMsg.classList.add('show-error-msg');
-        setTimeout(function(){
-            errorMsg.classList.remove('show-error-msg');
-        }, 3000);
-    }
+        if(peopleValue <= 0){
+            errorMsg.classList.add('show-error-msg');
+            setTimeout(function(){
+                errorMsg.classList.remove('show-error-msg');
+            }, 3000);
+        }
 
-    calculateTip();
-});
+        calculateTip();
+}
 
 function calculateTip(){
     if (peopleValue >=1 ){
@@ -106,7 +108,7 @@ function calculateTip(){
 
 //Reset button
 function reset(){
-    bill.value = '';
+    bill.value = '0';
     setBillValue();
 
     tipBtns[2].click();
